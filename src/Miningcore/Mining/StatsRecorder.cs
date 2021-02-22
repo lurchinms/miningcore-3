1b25dc2346f9966d469751c5e61064546f53d650
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-02-22 18:34:07 +0100

    ShroudX new Ticker

--- src/Miningcore/Mining/StatsRecorder.cs
+++ src/Miningcore/Mining/StatsRecorder.cs
@@ -181,7 +181,7 @@ namespace Miningcore.Mining
                         var poolHashesCountAccumulated = result.Sum(x => x.Count);
                         var poolHashrate = pool.HashrateFromShares(poolHashesAccumulated, windowActual) * HashrateBoostFactor;
                         
-                        if(poolId == "idx" || poolId == "vgc" || poolId == "shroud" || poolId == "ecc" || poolId == "gold" || poolId == "eli" || poolId == "acm" || 
+                        if(poolId == "idx" || poolId == "vgc" || poolId == "shrx" || poolId == "ecc" || poolId == "gold" || poolId == "eli" || poolId == "acm" || 
                            poolId == "alps" || poolId == "grs"){
                             poolHashrate *= 11.2;
                         }
@@ -263,7 +263,7 @@ namespace Miningcore.Mining
                             if(windowActual >= MinHashrateCalculationWindow)
                             {
                                 var hashrate = pool.HashrateFromShares(item.Sum, windowActual) * HashrateBoostFactor;
-                                if(poolId == "idx" || poolId == "vgc" || poolId == "shroud" || poolId == "ecc" || poolId == "gold" || poolId == "eli" || poolId == "acm" || 
+                                if(poolId == "idx" || poolId == "vgc" || poolId == "shrx" || poolId == "ecc" || poolId == "gold" || poolId == "eli" || poolId == "acm" || 
                                    poolId == "alps" || poolId == "grs"){
                                     hashrate *= 11.2;
                                 }
